3002ebda13c3057393f03755c24245215459dd6e
Joker2387089590 <2387089590@qq.com>
2022-04-30 08:38:57 +0800

    add EasyFmt, update QDebugHooker impl

--- QDebugHooker.h
+++ QDebugHooker.h
@@ -8,6 +8,9 @@ struct Detecter : std::false_type {};
 template <typename T, template<typename> typename Op>
 struct Detecter<T, Op, std::void_t<Op<T>>> : std::true_type { using type = T; };
 
+template<typename T, template<typename> typename... Ops>
+inline static constexpr bool detectResult = std::conjunction_v<Detecter<T, Ops>...>;
+
 template<typename T>
 struct QDebugTester
 {
@@ -15,23 +18,15 @@ struct QDebugTester
 	static constexpr auto f = static_cast<QDebug& (QDebug::*)(S)>(&QDebug::operator<<);
 
 	template<typename R> using Origin   = decltype(f<R>);
-	template<typename R> using Const    = decltype(f<const R>);
-	template<typename R> using Ref      = decltype(f<R&>);
-	template<typename R> using ConstRef = decltype(f<const R&>);
+	template<typename R> using Const    = decltype(f<std::add_const_t<R>>);
+	template<typename R> using Ref      = decltype(f<std::add_lvalue_reference_t<R>>);
+	template<typename R> using ConstRef = decltype(f<std::add_lvalue_reference_t<std::add_const_t<R>>>);
 
 	template<typename P> using PtrTrait = std::enable_if_t<std::is_pointer_v<P>, void>;
 	template<typename P> using ConstPtr = decltype(f<const PtrTrait<P>*>);
 
-	using type =
-		std::conditional_t<Detecter<T, Origin>::value,   typename Detecter<T, Origin>::type,
-		std::conditional_t<Detecter<T, Const>::value,    typename Detecter<T, Const>::type,
-		std::conditional_t<Detecter<T, Ref>::value,      typename Detecter<T, Ref>::type,
-		std::conditional_t<Detecter<T, ConstRef>::value, typename Detecter<T, Ref>::type,
-		std::conditional_t<Detecter<T, ConstPtr>::value, typename Detecter<T, ConstPtr>::type,
-		void>>>>>;
-
-    static constexpr bool hasOp = !std::is_same_v<type, void>;
+	static constexpr bool hasOp = detectResult<T, Origin, Const, Ref, ConstRef, ConstPtr>;
 };
 
 template<typename T>
-inline constexpr bool isQDebugSupportedType = QDebugTester<T>::hasOp;
+inline constexpr bool isQDebugSupportedType = QDebugTester<std::decay_t<T>>::hasOp;

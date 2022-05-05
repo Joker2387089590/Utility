32703e2a664fceb9f55d18d7802fec81b160a7c7
Joker2387089590 <2387089590@qq.com>
2022-05-05 14:22:33 +0800

    add Access, QDebugHooker add debug

--- QDebugHooker.h
+++ QDebugHooker.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <type_traits>
 #include <QDebug>
 
 namespace QDebugTest
@@ -19,41 +18,54 @@ using F = decltype(static_cast<QDebug& (QDebug::*)(S)>(&QDebug::operator<<));
 // Detector::Op, decorate F to make a 'QDebug& (QDebug::*)(T const&?)' type
 struct Ops
 {
-template<typename T> using Origin   = F<T>;
-template<typename T> using Const    = F<T const>;
-template<typename T> using Ref      = F<T &>;
-template<typename T> using ConstRef = F<T const&>;
-// Any pointer except 'const char*' is to 'const void*'
-template<typename P>
-using TraitPtr = std::enable_if_t<std::is_pointer_v<P>, const void*>;
-template<typename P> using ConstPtr = F<TraitPtr<P>>;
+	template<typename T> using Origin   = F<T>;
+	template<typename T> using Const    = F<T const>;
+	template<typename T> using Ref      = F<T &>;
+	template<typename T> using ConstRef = F<T const&>;
+
+	// Any pointer except 'const char*' is to 'const void*'
+	template<typename P>
+	using TraitPtr = std::enable_if_t<std::is_pointer_v<P>, const void*>;
+	template<typename P> using ConstPtr = F<TraitPtr<P>>;
 };
 
 // Test Ds... one-by-one
-template<typename T, template<typename> typename Op, template<typename> typename... Ops>
+template<typename T, template<typename> typename Op,
+		 template<typename> typename... Ops>
 struct Expander
 {
 	using D = Detector<T, Op>;
 	using type = std::conditional_t<D::value, typename D::type, typename Expander<T, Ops...>::type>;
 };
+
 template<typename T, template<typename> typename Op>
 struct Expander<T, Op>
 {
 	using type = typename Detector<T, Op>::type;
 };
+
 template<typename T, template<typename> typename... Ds>
-using typex = typename Expander<T, Ds...>::type;
+using ExType = typename Expander<T, Ds...>::type;
 }
 
 template<typename T>
 struct Tester : private Detail::Ops
 {
-	using type = Detail::typex<std::decay_t<T>, Origin, Const, Ref, ConstRef, ConstPtr>;
-	constexpr operator bool() const { return !std::is_same_v<type, void>; }
+	using type = Detail::ExType<std::decay_t<T>, Origin, Const, Ref, ConstRef, ConstPtr>;
+	static constexpr bool value = !std::is_same_v<type, void>;
+	constexpr operator bool() const { return value; }
+	constexpr bool operator()() const { return value; }
 };
 
 template<typename T>
-inline constexpr bool isQDebugSupportedType = QDebugTest::Tester<T>{};
+inline constexpr bool isQDebugSupportedType = QDebugTest::Tester<T>::value;
+
+template<typename... Ts>
+QString debug(Ts&&... ts)
+{
+	QString buf;
+	(QDebug(&buf).nospace().noquote() << ... << ts);
+	return buf;
 }
 
-using QDebugTest::isQDebugSupportedType;
+}

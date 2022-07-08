ccdc37c1a34050d725e2aa8952c06a17be58d386
fuye.zhu <fuye.zhu@greentest.com.cn>
2022-07-08 09:20:53 +0800

    fix LazyGenerator

--- LazyGenerator.h
+++ LazyGenerator.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <utility> // std::forward
+#include <tuple>   // std::forward_as_tuple
 
 namespace Lazys
 {
@@ -13,6 +14,10 @@ public:
     /// 这个对象在遇到需要 func 的返回值时，调用 generator 生成
     operator decltype(auto)() { return generator(); }
     operator decltype(auto)() const { return generator(); }
+
+    /// 主动生成需要的值
+    decltype(auto) operator()() { return generator(); }
+    decltype(auto) operator()() const { return generator(); }
 
 private:
 	F generator;
@@ -29,14 +34,22 @@ struct CtorTag
 template<typename T, typename... Args>
 auto lazy(Args&&... args)
 {
-    return Lazy([&] { return T{std::forward<Args>(args)...}; });
+    return Lazy([as = std::forward_as_tuple(std::forward<Args>(args)...)] {
+        return std::apply([](auto&&... as) {
+            return T{std::forward<Args>(as)...};
+        }, as);
+    });
 }
 
 /// Tag Dispatch，在 try_emplace 时调用括号形式的构造函数
 template<typename T, typename... Args>
 auto lazy(CtorTag, Args&&... args)
 {
-    return Lazy([&] { return T(std::forward<Args>(args)...); });
+    return Lazy([as = std::forward_as_tuple(std::forward<Args>(args)...)] {
+        return std::apply([](auto&&... as) {
+            return T(std::forward<Args>(as)...);
+        }, as);
+    });
 }
 }
 

38bce7e4b1e81ca269cc73431eb375be091fd0a5
Joker2387089590 <2387089590@qq.com>
2022-10-22 00:05:05 +0800

    TypeList add to rows, add slice, add ifAll & ifOne, add Unite

--- TypeList.h
+++ TypeList.h
@@ -60,7 +60,11 @@ struct ReverseTrait<> { using type = TList<>; };
 template<std::size_t i, typename... Ts> struct AtTrait;
 
 template<std::size_t i, typename T, typename... Ts>
-struct AtTrait<i, T, Ts...> { using type = typename AtTrait<i - 1, Ts...>::type; };
+struct AtTrait<i, T, Ts...>
+{
+	static_assert(i <= sizeof...(Ts), "The index of At is out of range!");
+	using type = typename AtTrait<i - 1, Ts...>::type;
+};
 
 template<typename T, typename... Ts>
 struct AtTrait<0, T, Ts...> { using type = T; };
@@ -99,12 +103,110 @@ struct UniqueTrait<TList<T...>, TList<>>
 	using type = TList<T...>;
 };
 
+/// to rows
+template<std::size_t c, typename Tused, typename Tend, typename... Ts>
+struct ToRowsTrait;
+
+template<std::size_t c, typename Tused, typename Tend, typename Ti, typename... Ts>
+struct ToRowsTrait<c, Tused, Tend, Ti, Ts...>
+{
+	static_assert(c > 0);
+	using Tnew = typename Tend::template Append<Ti>;
+	using type = std::conditional_t<
+		Tnew::size() == c,
+		typename ToRowsTrait<c, typename Tused::template Append<Tnew>, TList<>, Ts...>::type,
+		typename ToRowsTrait<c, Tused, Tnew, Ts...>::type
+	>;
+};
+
+template<std::size_t c, typename Tused, typename Tend>
+struct ToRowsTrait<c, Tused, Tend>
+{
+	using type = std::conditional_t<
+		Tend::size() == 0,
+		Tused,
+		typename Tused::template Append<Tend>
+	>;
+};
+
+/// slice
+template<typename TL, std::size_t b, typename Indexes>
+struct SliceTrait;
+
+template<typename TL, std::size_t b, std::size_t... is>
+struct SliceTrait<TL, b, std::index_sequence<is...>>
+{
+	using type = typename TL::template Select<(b + is)...>;
+};
+
+/// remove at
+template<typename TL, std::size_t... rs>
+struct RemoveAtTrait
+{
+	static_assert(std::conjunction_v<std::bool_constant<rs < TL::size()>...>,
+				  "remove an index that out of TList's range!");
+
+	template<std::size_t... us, std::size_t i, std::size_t... is>
+	static auto indexFilter(std::index_sequence<us...>, std::index_sequence<i, is...>)
+	{
+		if constexpr (std::disjunction_v<std::bool_constant<rs == i>...>)
+			return indexFilter(std::index_sequence<us...>{}, std::index_sequence<is...>{});
+		else
+			return indexFilter(std::index_sequence<us..., i>{}, std::index_sequence<is...>{});
+	}
+
+	template<std::size_t... us>
+	static auto indexFilter(std::index_sequence<us...> u, std::index_sequence<>)
+	{
+		return select(u);
+	}
+
+	template<std::size_t... us>
+	static auto select(std::index_sequence<us...>)
+	{
+		return typename TL::template Select<us...>{};
+	}
+
+	static auto begin()
+	{
+		auto us = std::index_sequence<>{};
+		auto is = std::make_index_sequence<TL::size()>{};
+		return indexFilter(us, is);
+	}
+};
+
 /// TList
 template<typename... Ts>
 struct TList
 {
 	constexpr TList() noexcept = default;
 	template<typename... Ti> explicit constexpr TList(Ti&&...) noexcept : TList() {}
+
+	static constexpr std::size_t size() { return sizeof...(Ts); }
+
+	// TList<T0, T1, ..., Ti, ...> => Ti
+	template<std::size_t i>
+	using At = typename AtTrait<i, Ts...>::type;
+
+	static constexpr bool empty() { return size() == 0; }
+
+	using First = At<0>;
+
+	using Last = At<size() - 1>;
+
+	// TList<T1, T2, T3>::Select<0, 2> => TList<T1, T3>
+	template<std::size_t... is>
+	using Select = TList<At<is>...>;
+
+	// TList<T1, T2, T3, T4>::Slice<0, 2> => TList<T1, T2, T3>
+	template<std::size_t b, std::size_t e = size()>
+	using Slice = typename SliceTrait<TList, b, std::make_index_sequence<e - b>>::type;
+
+	template<typename... Tx>
+	using Prepend = TList<Tx..., Ts...>;
+
+	template<typename... Tx>
+	using Append = TList<Ts..., Tx...>;
 
 	// F: struct { using type = 'Ts'?; } => F<Ts>::type
 	template<template<typename...> typename F>
@@ -114,20 +216,25 @@ struct TList
 	template<template<typename...> typename F>
 	using Apply = F<Ts...>;
 
-	template<typename... Tx>
-	using Prepend = TList<Tx..., Ts...>;
-
-	template<typename... Tx>
-	using Append = TList<Ts..., Tx...>;
+	// TList<T0, T1, T2> => TList<T2, T1, T0>
+	using Reverse = typename ReverseTrait<Ts...>::type;
 
 	template<std::size_t s>
 	using Repeat = typename RepeatTrait<s, TList<>, TList>::type;
 
+	// TList<T1, T1, T2, T3, T3> => TList<T1, T2, T3>
 	using Unique = typename UniqueTrait<TList<>, TList>::type;
+
+	// TList<T1, T2>::Unite<TList<T2, T3>> => TList<T1, T2, T3>
+	template<typename... To>
+	using Unite = typename TList<Ts..., To...>::Unique;
 
 	using RemoveFirst = typename RemoveFirstTrait<Ts...>::type;
 
 	using RemoveLast = typename RemoveLastTrait<Ts...>::type;
+
+	template<std::size_t... rs>
+	using RemoveAt = decltype(RemoveAtTrait<TList, rs...>::begin());
 
 	// C: if (C<Ts>::value) remove(Ts)
 	template<template<typename...> typename C>
@@ -136,21 +243,6 @@ struct TList
 	// Tx: if(Ts == Tx) remove(Ts)
 	template<typename Tx>
 	using Remove = RemoveIf<IsSameTrait<Tx>::template IsSame>;
-
-	// TList<T0, T1, T2> => TList<T2, T1, T0>
-	using Reverse = typename ReverseTrait<Ts...>::type;
-
-	// T0, T1, ..., Ti, ... => Ti
-	template<std::size_t i>
-	using At = typename AtTrait<i, Ts...>::type;
-
-	static constexpr std::size_t size() { return sizeof...(Ts); }
-
-	static constexpr bool empty() { return size() == 0; }
-
-	using First = At<0>;
-
-	using Last = At<size() - 1>;
 
 	static constexpr std::size_t npos = -1;
 
@@ -173,6 +265,16 @@ struct TList
 		std::size_t ri = Reverse::template find<T>();
 		return ri == npos ? npos : size() - ri - 1;
 	}
+
+	// TList<T1, T2, T3, T4>::ToRows<2> => TList<TList<T1, T2>, TList<T3, T4>>
+	template<std::size_t column>
+	using ToRows = typename ToRowsTrait<column, TList<>, TList<>, Ts...>::type;
+
+	template<template<typename...> typename Is>
+	static constexpr bool ifAll = std::conjunction_v<Is<Ts>...>;
+
+	template<template<typename...> typename Is>
+	static constexpr bool ifOne = std::disjunction_v<Is<Ts>...>;
 };
 
 /// Empty TList
@@ -242,7 +344,6 @@ struct MergeTrait<TList<T1...>, TList<T2...>>
 	using type = TList<T1..., T2...>;
 };
 
-// Merge<T1, TList<T2, T3, T4>, TList<T5, T6>> => TList<T1, T2, T3, T4, T5, T6>
 template<typename... Ts>
 using Merge = typename MergeTrait<Ts...>::type;
 
@@ -250,7 +351,6 @@ using Merge = typename MergeTrait<Ts...>::type;
 template<template<typename...> typename T, typename... Ts>
 auto fromTrait(T<Ts...>*) -> TList<Ts...>;
 
-// std::tuple<T1, T2, T3, ...> => TList<T1, T2, T3, ...>
 template<typename T>
 using From = decltype(fromTrait(std::add_pointer_t<std::decay_t<T>>{}));
 
@@ -271,17 +371,22 @@ struct ZipTrait<TList<Ts...>>
 
 template<typename Ts> struct ZipTraitHelper;
 
-template<typename... Ts>
-struct ZipTraitHelper<TList<Ts...>>
+template<typename... TLs>
+struct ZipTraitHelper<TList<TLs...>>
 {
+	template<typename TL, typename T1>
+	using Prepend = typename TL::template Prepend<T1>;
+
 	template<typename... T1>
-	using type = TList<Merge<TList<T1>, Ts>...>;
+	using type = TList<Prepend<TLs, T1>...>;
 };
 
-template<typename... T1, typename... Ts>
-struct ZipTrait<TList<T1...>, Ts...>
+template<typename... T1, typename... TLs>
+struct ZipTrait<TList<T1...>, TLs...>
 {
-	using BaseType = typename ZipTrait<Ts...>::type;
+	using BaseType = typename ZipTrait<TLs...>::type;
+	static_assert(sizeof...(T1) == BaseType::size(),
+				  "Zip requires all input TLists have the same size!");
 	using type = typename ZipTraitHelper<BaseType>::template type<T1...>;
 };
 
@@ -291,8 +396,15 @@ using Zip = typename ZipTrait<Ts...>::type;
 
 namespace Types
 {
+/// variadic type list
 using Detail::TList;
+
+/// Merge<T1, TList<T2, T3, T4>, TList<T5, T6>> => TList<T1, T2, T3, T4, T5, T6>
 using Detail::Merge;
+
+/// any_template<T1, T2, T3, ...> => TList<T1, T2, T3, ...>
 using Detail::From;
+
+/// Zip<TList<T1, T2, T3>, TList<T4, T5, T6>> => TList<TList<T1, T4>, TList<T2, T5>, TList<T3, T6>>
 using Detail::Zip;
 }

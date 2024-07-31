978000332311c0c0d3989e678174021e6224db15
Joker <joker.lin@greentest.com.cn>
2024-07-31 18:45:39 +0800

    [protobuf] improve indent

--- include/Utility/ProtobufQt.h
+++ include/Utility/ProtobufQt.h
@@ -21,101 +21,107 @@ namespace ProtobufQt
 struct JsonConverter
 {
 	static QJsonValue toQt(const google::protobuf::Value& v){
-    using google::protobuf::Value;
-    switch(v.kind_case())
-    {
-    case Value::kNullValue:   return QJsonValue::Type::Null;
-    case Value::kNumberValue: return v.number_value();
-    case Value::kStringValue: return QString::fromStdString(v.string_value());
-    case Value::kBoolValue:   return v.bool_value();
-    case Value::kStructValue: return toQt(v.struct_value());
-    case Value::kListValue:   return toQt(v.list_value());
-	case Value::KIND_NOT_SET:
-	default:
-		return QJsonValue::Type::Undefined;
-    }
-}
+		using google::protobuf::Value;
+		switch(v.kind_case())
+		{
+		case Value::kNullValue:   return QJsonValue::Type::Null;
+		case Value::kNumberValue: return v.number_value();
+		case Value::kStringValue: return QString::fromStdString(v.string_value());
+		case Value::kBoolValue:   return v.bool_value();
+		case Value::kStructValue: return toQt(v.struct_value());
+		case Value::kListValue:   return toQt(v.list_value());
+		case Value::KIND_NOT_SET:
+		default:
+			return QJsonValue::Type::Undefined;
+		}
+	}
 	static QJsonObject toQt(const google::protobuf::Struct& obj){
-    QJsonObject qjson;
-    for(auto& [k, v] : obj.fields())
-        qjson.insert(QString::fromStdString(k), toQt(v));
-    return qjson;
-}
+		QJsonObject qjson;
+		for(auto& [k, v] : obj.fields())
+			qjson.insert(QString::fromStdString(k), toQt(v));
+		return qjson;
+	}
 	static QJsonArray toQt(const google::protobuf::ListValue& arr){
-    QJsonArray qjson;
-    for(auto& v : arr.values()) qjson.append(toQt(v));
-	return qjson;
-}
+		QJsonArray qjson;
+		for(auto& v : arr.values()) qjson.append(toQt(v));
+		return qjson;
+	}
 
 	static void toProtobuf(const QJsonObject& obj, google::protobuf::Struct* s){
-	const auto b = obj.begin();
-	const auto e = obj.end();
-	auto fields = s->mutable_fields();
-	for(auto i = b; i != e; ++i)
-	{
-		auto [pos, success] = fields->try_emplace(i.key().toStdString());
-		if(!success)
-			[[unlikely]] continue;
-		else
-			toProtobuf(i.value(), &pos->second);
+		const auto b = obj.begin();
+		const auto e = obj.end();
+		auto fields = s->mutable_fields();
+		for(auto i = b; i != e; ++i)
+		{
+			auto [pos, success] = fields->try_emplace(i.key().toStdString());
+			if(!success)
+				[[unlikely]] continue;
+			else
+				toProtobuf(i.value(), &pos->second);
+		}
 	}
-}
-	static void toProtobuf(const QJsonArray& arr, google::protobuf::ListValue* list){
-	list->clear_values();
-	const auto b = arr.begin();
-	const auto e = arr.end();
-	for(auto i = b; i != e; ++i) toProtobuf(*i, list->add_values());
-}
-	static void toProtobuf(const QJsonValue& qv, google::protobuf::Value* pv){
-	switch(qv.type())
+
+	static void toProtobuf(const QJsonArray& arr, google::protobuf::ListValue* list)
 	{
-	case QJsonValue::Null: [[unlikely]]
-		pv->set_null_value({});
-		break;
-	case QJsonValue::Bool:
-		pv->set_bool_value(qv.toBool());
-		break;
-	case QJsonValue::Double:
-		pv->set_number_value(qv.toDouble());
-		break;
-	case QJsonValue::String:
-		pv->set_string_value(qv.toString().toStdString());
-		break;
-	case QJsonValue::Array:
-		toProtobuf(qv.toArray(), pv->mutable_list_value());
-		break;
-	case QJsonValue::Object:
-		toProtobuf(qv.toObject(), pv->mutable_struct_value());
-		break;
-	case QJsonValue::Undefined: [[unlikely]]
-		break;
+		list->clear_values();
+		const auto b = arr.begin();
+		const auto e = arr.end();
+		for(auto i = b; i != e; ++i) toProtobuf(*i, list->add_values());
 	}
-}
-	static void toProtobuf(QJsonValueConstRef qv, google::protobuf::Value* pv){
-	switch(qv.type())
+
+	static void toProtobuf(const QJsonValue& qv, google::protobuf::Value* pv)
 	{
-	case QJsonValue::Null: [[unlikely]]
-		pv->set_null_value({});
-		break;
-	case QJsonValue::Bool:
-		pv->set_bool_value(qv.toBool());
-		break;
-	case QJsonValue::Double:
-		pv->set_number_value(qv.toDouble());
-		break;
-	case QJsonValue::String:
-		pv->set_string_value(qv.toString().toStdString());
-		break;
-	case QJsonValue::Array:
-		toProtobuf(qv.toArray(), pv->mutable_list_value());
-		break;
-	case QJsonValue::Object:
-		toProtobuf(qv.toObject(), pv->mutable_struct_value());
-		break;
-	case QJsonValue::Undefined: [[unlikely]]
-		break;
+		switch(qv.type())
+		{
+		case QJsonValue::Null: [[unlikely]]
+			pv->set_null_value({});
+			break;
+		case QJsonValue::Bool:
+			pv->set_bool_value(qv.toBool());
+			break;
+		case QJsonValue::Double:
+			pv->set_number_value(qv.toDouble());
+			break;
+		case QJsonValue::String:
+			pv->set_string_value(qv.toString().toStdString());
+			break;
+		case QJsonValue::Array:
+			toProtobuf(qv.toArray(), pv->mutable_list_value());
+			break;
+		case QJsonValue::Object:
+			toProtobuf(qv.toObject(), pv->mutable_struct_value());
+			break;
+		case QJsonValue::Undefined: [[unlikely]]
+			break;
+		}
 	}
-}
+	
+	static void toProtobuf(QJsonValueConstRef qv, google::protobuf::Value* pv)
+	{
+		switch(qv.type())
+		{
+		case QJsonValue::Null: [[unlikely]]
+			pv->set_null_value({});
+			break;
+		case QJsonValue::Bool:
+			pv->set_bool_value(qv.toBool());
+			break;
+		case QJsonValue::Double:
+			pv->set_number_value(qv.toDouble());
+			break;
+		case QJsonValue::String:
+			pv->set_string_value(qv.toString().toStdString());
+			break;
+		case QJsonValue::Array:
+			toProtobuf(qv.toArray(), pv->mutable_list_value());
+			break;
+		case QJsonValue::Object:
+			toProtobuf(qv.toObject(), pv->mutable_struct_value());
+			break;
+		case QJsonValue::Undefined: [[unlikely]]
+			break;
+		}
+	}
 };
 
 class anyToQJson

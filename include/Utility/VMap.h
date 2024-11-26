edf6d4ca65a0cea3c96298eb13fa9c875af55ca5
fuye.zhu <fuye.zhu@greentest.com.cn>
2024-11-26 19:29:23 +0800

    修改VMap引用问题

--- include/Utility/VMap.h
+++ include/Utility/VMap.h
@@ -6,7 +6,7 @@ template<typename Q>
 class VMap
 {
 public:
-	template<typename Qx> explicit VMap(Qx&& m) : m(static_cast<Q>(m)) {}
+	template<typename Qx> explicit VMap(Qx&& m) : m(static_cast<Qx&&>(m)) {}
 	auto begin()  const { return m.keyValueBegin(); }
 	auto end()    const { return m.keyValueEnd(); }
 	auto cbegin() const { return std::as_const(m).keyValueBegin(); }
@@ -16,9 +16,9 @@ private:
 };
 
 template<typename K, typename V> VMap(QMap<K, V>      &) -> VMap<QMap<K, V>&>;
-template<typename K, typename V> VMap(QMap<K, V>     &&) -> VMap<QMap<K, V>&&>;
+template<typename K, typename V> VMap(QMap<K, V>     &&) -> VMap<QMap<K, V>>;
 template<typename K, typename V> VMap(QMap<K, V> const&) -> VMap<QMap<K, V> const&>;
 
 template<typename K, typename V> VMap(QHash<K, V>      &) -> VMap<QHash<K, V>&>;
-template<typename K, typename V> VMap(QHash<K, V>     &&) -> VMap<QHash<K, V>&&>;
+template<typename K, typename V> VMap(QHash<K, V>     &&) -> VMap<QHash<K, V>>;
 template<typename K, typename V> VMap(QHash<K, V> const&) -> VMap<QHash<K, V> const&>;
